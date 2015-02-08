44ad713818f4e41d7c3185821bcbe4d7f00c64ed
barif <w3db@yandex.ru>
2015-02-08 15:11:29 +0200

    implement config var

--- example.js
+++ example.js
@@ -1,6 +1,12 @@
 var http = require('http');
+var config = {
+    host: '192.168.1.190',
+    port: 1337
+};
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
-}).listen(1337, '192.168.1.190');
-console.log('Server running at http://192.168.1.190:1337/');
+}).listen(config.port, config.host);
+
+console.log('Server running at http://' + config.host + ':' + config.port + '/');
