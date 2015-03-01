7b155994afae543282d4045faf99bf1fa4b9cd23
barif <w3db@yandex.ru>
2015-03-01 10:35:30 +0200

    http collect

--- src/learnyounode/httpcollect.js
+++ src/learnyounode/httpcollect.js
@@ -1,1 +1,14 @@
 var bl = require('bl');
+var http = require('http');
+
+http.get(process.argv[2], function (response) {
+    response.setEncoding('utf8');
+
+    response.pipe(bl(function (err, data) {
+        if(err) {
+            console.error(err);
+        }
+
+        console.log(data.toString());
+    }));
+});
--- src/learnyounode/module/program.js
+++ src/learnyounode/module/program.js
@@ -1,11 +1,7 @@
 var mymodule = require('./mymodule.js');
 
 function moduleCallback(error, output) {
-    if(error) {
-        console.log(error);
-    } else {
-        console.log(output);
-    }
+    (error) ? console.error(error) : console.log(output);
 }
 
 mymodule(process.argv[2], process.argv[3], moduleCallback);
