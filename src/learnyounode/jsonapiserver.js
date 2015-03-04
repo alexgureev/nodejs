3837b63746e6cf808486c85a121c5ed1550277fa
barif <w3db@yandex.ru>
2015-03-04 22:36:23 +0200

    json api server

---
+++ src/learnyounode/jsonapiserver.js
@@ -0,0 +1,39 @@
+var http = require('http')
+var url = require('url');
+
+var server = http.createServer(function (req, res) {
+    var Url = url.parse(req.url, true);
+    var jsonResponse = {};
+
+    switch(Url.pathname) {
+        case '/api/parsetime' : {
+            var dateObject = new Date(Url.query.iso);
+
+            jsonResponse = {
+                "hour": dateObject.getHours(),
+                "minute": dateObject.getMinutes(),
+                "second": dateObject.getSeconds()
+            };
+
+            break;
+        }
+
+        case '/api/unixtime' : {
+
+            var dateObject = new Date(Url.query.iso);
+            console.log(dateObject.getHours());
+
+            jsonResponse = {
+                "unixtime": Math.round(dateObject.getTime())
+            };
+
+            break;
+        }
+    }
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(jsonResponse));
+
+})
+
+server.listen(Number(process.argv[2]));
