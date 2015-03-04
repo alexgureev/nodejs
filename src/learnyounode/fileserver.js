e4a46ac84647434bd19fd316d5debe39d0b8c63c
barif <w3db@yandex.ru>
2015-03-04 21:40:48 +0200

    fileserver

--- src/learnyounode/fileserver.js
+++ src/learnyounode/fileserver.js
@@ -1,9 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 
-var server = http.createServer(function (req, res) {
-
-    fs.createReadStream()
+var server = http.createServer(function (request, response) {
+    var stream = fs.createReadStream(process.argv[3]);
+    stream.pipe(response);
 });
 
 server.listen(process.argv[2]);
