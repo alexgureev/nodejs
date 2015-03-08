ea303591a253f610277a60c7ab95f26e14aade01
barif <w3db@yandex.ru>
2015-03-08 21:02:50 +0200

    express js

--- src/express/Express.js
+++ src/express/Express.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-app.get('/', function(req, res) {
-    res.end('Hello World!')
+var express = require('express')
+var app = express()
+app.get('/home', function(req, res) {
+  res.end('Hello World!')
 })
-app.listen(process.argv[2]);
+app.listen(process.argv[2])
