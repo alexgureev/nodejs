da612ff742caffa61b61f05e7799a1503e02e983
barif <w3db@yandex.ru>
2015-03-09 23:15:23 +0200

    controller + tests

--- src/node-demo-app/app/main.js
+++ src/node-demo-app/app/main.js
@@ -1,10 +1,12 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    pages = require(__dirname + '/controllers/pages');
 
+// configuration settings 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
-    res.send('Hello, World!')
-});
+// mount routes
+app.get('/', function (req, res) { res.redirect('/home') });
+app.get('/home', pages.home);
 
 module.exports = app;
