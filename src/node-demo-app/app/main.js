7554ef81679f227f65865cbeb16fc723cc38a7fe
barif <barif@syneforge.com>
2015-03-10 19:07:53 +0200

    add bootstrapper, about template

--- src/node-demo-app/app/main.js
+++ src/node-demo-app/app/main.js
@@ -1,14 +1,23 @@
 var express = require('express')
+    , ejsLocals = require('ejs-locals')
     , app = express()
     , pages = require(__dirname + '/controllers/pages');
 
 // configuration settings 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.engine('ejs', ejsLocals)
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'));
 
-// mount routes
-app.get('/', function (req, res) { res.redirect('/home') });
-app.get('/home', pages.home);
+// set view locals
+app.use(function (req, res, next) {
+    app.locals.route = req.url
+    next()
+})
 
-module.exports = app;
+// mount routes
+app.get('/', function (req, res) { res.redirect('/home') })
+app.get('/home', pages.home)
+app.get('/about', pages.about)
+
+module.exports = app
