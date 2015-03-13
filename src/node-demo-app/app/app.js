5d330466d3a277536e825dadc31bbfc162a2b778
barif <w3db@yandex.ru>
2015-03-13 23:32:44 +0200

    configuration file

--- src/node-demo-app/app/app.js
+++ src/node-demo-app/app/app.js
@@ -1,36 +1,21 @@
 var express = require('express'),
     ejsLocals = require('ejs-locals'),
     app = express(),
-    pages = require(__dirname + '/controllers/pages'),
-    quotes = require(__dirname + '/controllers/quotes'),
-    account = require(__dirname + '/controllers/account'),
+    config = require(__dirname + '/config.js'),
+    pages = require(__dirname + config.path.pages),
+    quotes = require(__dirname + config.path.quotes),
+    account = require(__dirname + config.path.account),
     bodyParser = require('body-parser'),
-    expressSession = require('express-session'),
-    RedisStore = require('connect-redis')(expressSession),
     cookieParser = require('cookie-parser');
 
 // configuration settings 
 app.engine('ejs', ejsLocals);
-app.set('views', __dirname + '/views');
+app.set('views', __dirname + config.path.views);
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/../public'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + config.path.public));
+app.use(bodyParser.urlencoded(config.bodyParser));
 app.use(cookieParser());
-app.use(expressSession({
-    store: new RedisStore({
-        host: '127.0.0.1',
-        port: 6379,
-        db: 0
-    }),
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        path: '/',
-        httpOnly: true,
-        secure: false,
-        maxAge: 360000 }
-}));
+app.use(config.sessionObj);
 
 // set view locals
 app.use(function (req, res, next) {
---
+++ src/node-demo-app/app/config.js
@@ -0,0 +1,36 @@
+(function () {
+    "use strict";
+    var expressSession = require('express-session'),
+        RedisStore = require('connect-redis')(expressSession);
+
+    module.exports = {
+        path: {
+            pages: "/controllers/pages",
+            quotes: "/controllers/quotes",
+            account: "/controllers/account",
+            views: "/views",
+            public: "/../public"
+        },
+        listen: [
+            {port: 8001, host: '0.0.0.0'}
+        ],
+        bodyParser: {extended: false},
+        sessionObj: expressSession({
+            store: new RedisStore({
+                host: '127.0.0.1',
+                port: 6379,
+                db: 0
+            }),
+            secret: 'keyboard cat',
+            resave: false,
+            saveUninitialized: true,
+            cookie: {
+                path: '/',
+                httpOnly: true,
+                secure: false,
+                maxAge: 360000
+            }
+        })
+    };
+})();
+
