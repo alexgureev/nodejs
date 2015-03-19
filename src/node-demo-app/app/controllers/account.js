4a594f867516ced55e47f942c4a9cabc662a773a
barif <w3db@yandex.ru>
2015-03-19 21:34:31 +0200

    cookies usage

--- src/node-demo-app/app/controllers/account.js
+++ src/node-demo-app/app/controllers/account.js
@@ -20,3 +20,24 @@ exports.countViews = function(req, res, next) {
         res.end('welcome to the session demo. refresh!')
     }
 }
+
+exports.forgot = function (req, res) {
+    //check
+    if (req.cookies.remember) {
+        res.send('Remembered :). Click to <a href="/forget">forget</a>!.');
+    } else {
+        res.send('<form method="post"><p>Check to <label>'
+        + '<input type="checkbox" name="remember"/> remember me</label> '
+        + '<input type="submit" value="Submit"/>.</p></form>');
+    }
+
+    //set
+    var minute = 60 * 1000;
+
+    if (req.body.remember) {
+        res.cookie('remember', 1, {maxAge: minute});
+    }
+
+    // delete
+    res.clearCookie('remember');
+}
